242eddb6c1dc53df45fab223dcca4f1e90e7da34
Vastin He <vastin@gmail.com>
2014-02-06 18:29:25 +0800

    add more.

---
+++ ex27.py
@@ -0,0 +1,33 @@
+not False
+not True
+
+True or False
+True or True
+False or True
+False or False
+
+True and False
+True and True
+False and True
+False and False
+
+not (True or False)
+not (True or True)
+not (False or True)
+not (False or False)
+
+not (True and False)
+not (True and True)
+not (False and True)
+not (False and False)
+
+1 != 0
+1 != 1
+0 != 1
+0 != 0
+
+1 == 0
+1 == 1
+0 == 1
+0 == 0
+
